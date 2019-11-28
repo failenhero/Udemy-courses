834b259eaab706f233a99c540591000297c1424b
failenhero <ouvld@mail.ru>
2019-11-28 19:34:55 +0300

    timer was added

--- Yoga-App/js/script.js
+++ Yoga-App/js/script.js
@@ -34,4 +34,45 @@ window.addEventListener('DOMContentLoaded', function(){
             }
         }
     });
+
+    //Timer
+
+    let deadline = '2019-12-11';
+
+    function getTimeRemaining(endtime){
+        let remainedTime = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((remainedTime/1000) % 60),
+            minutes = Math.floor((remainedTime/1000/60) % 60),
+            hours = Math.floor(remainedTime/1000/60/60);
+            //hours = Math.floor((remainedTime/1000/60/60) % 24), -если у нас ещё есть дни, часы отображаются так
+            //days =  Math.floor(remainedTime/1000/60/60/24);
+        return {
+            'total' : remainedTime,
+            'seconds' : seconds,
+            'minutes' : minutes,
+            'hours' : hours
+        };
+    }
+
+    function setClock(id, endtime){
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+
+            hours.textContent = t.hours;
+            minutes.textContent = t.minutes;
+            seconds.textContent = t.seconds;
+
+            if (t.total <= 0){
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setClock('timer', deadline);
 });
