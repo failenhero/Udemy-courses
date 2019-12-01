4b598339ecd1b8e0a566261c312cb86f5377c507
failenhero <ouvld@mail.ru>
2019-12-01 21:32:50 +0300

    exercise 12 is done

---
+++ draft/exercise12.js
@@ -0,0 +1,20 @@
+//jshint esversion:6
+
+class Options {
+    constructor( height, width, bg, fontSize, textAlign){
+        this.height = height;
+        this.width = width;
+        this.bg = bg;
+        this.fontSize = fontSize;
+        this.textAlign = textAlign;
+    }
+    createDiv() {
+        let elem = document.createElement('div');
+        document.body.appendChild(elem);
+        let param = `height: ${this.height}px; width: ${this.width}px; background-color: ${this.bg}; font-size: ${this.fontSize}px; text-aligin: ${this.textAlign};`;
+        elem.style.cssText = param;
+    }
+}
+
+const item = new Options(200, 300, 'red', 14, 'center');
+item.createDiv();
