df6162c20b4dc725480dd8fead02d334999181a3
failenhero <ouvld@mail.ru>
2019-12-05 12:44:31 +0300

    form has been connected with server

--- Yoga-App/js/script.js
+++ Yoga-App/js/script.js
@@ -1,4 +1,4 @@
-//jshint esversion:6
+//jshint esversion: 6
 window.addEventListener('DOMContentLoaded', function(){
 
     'use strickt';
@@ -88,7 +88,7 @@ window.addEventListener('DOMContentLoaded', function(){
 
     setClock('timer', deadline);
 
-    //Модальное окно
+    //Modal window
 
     let btnMore = document.querySelector('.more'),
         btnDescription = document.querySelectorAll('.description-btn'),
@@ -112,4 +112,50 @@ window.addEventListener('DOMContentLoaded', function(){
         btnDescription[i].addEventListener('click', modalWindow);
     }
 
+    //Form
+
+    let message = {
+        loading: 'Загрузка...',
+        sucsess: 'Спасибо за заявку! Мы скоро с Вами свяжемся',
+        failure: 'Что-то пошло не так'
+    };
+
+    let form = document.querySelector('.main-form'),
+        input = form.getElementsByTagName('input'),
+        statusMessage = document.createElement('div');
+
+        statusMessage.classList.add('status');
+
+        form.addEventListener('submit', function(event){
+            event.preventDefault();
+            form.appendChild(statusMessage);
+
+            let request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+            request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+
+            let formData = new FormData(form);
+
+            let obj = {};
+            formData.forEach(function(value, key){
+                obj[key] = value;
+            });
+            let json = JSON.stringify(obj);
+
+            request.send(json);
+
+            request.addEventListener('readystatechange', function(){
+                if (request.readyState > 4) {
+                    statusMessage.innerHTML = message.loading;
+                } else if (request.readyState === 4 && request.status == 200) {
+                    statusMessage.innerHTML = message.sucsess;
+                } else {
+                    statusMessage.innerHTML = message.failure;
+                }
+            });
+
+            for (let i = 0; i < input.length; i++){
+                input[i].value = '';
+            }
+        });
 });
