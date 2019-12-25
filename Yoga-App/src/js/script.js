ecfab91a573b6092ab3afc7486cbedebc822dfc2
failenhero <ouvld@mail.ru>
2019-12-25 23:35:30 +0300

    babel & polyfills were used

--- Yoga-App/src/js/script.js
+++ Yoga-App/src/js/script.js
@@ -1,4 +1,7 @@
 //jshint esversion: 6
+
+require('es6-promise').polyfill();
+
 window.addEventListener('DOMContentLoaded', function(){
 
     'use strickt';
@@ -17,3 +20,21 @@ window.addEventListener('DOMContentLoaded', function(){
     timer();
 
 });
+
+if ('NodeList' in window && !NodeList.prototype.forEach) {
+
+    console.info('polyfill for IE11');
+
+    NodeList.prototype.forEach = function (callback, thisArg) {
+
+      thisArg = thisArg || window;
+
+      for (var i = 0; i < this.length; i++) {
+
+        callback.call(thisArg, this[i], i, this);
+
+      }
+
+    };
+
+  }
